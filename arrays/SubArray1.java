39013cc80d366bf476d8788abc8e7b4b6d9984fd
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-05-16 22:02:47 +0530

    Count sub arrays having sum less than k

---
+++ arrays/SubArray1.java
@@ -0,0 +1,46 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SubArray1 {
+	
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i =0; i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int k = sc.nextInt();
+
+        countSubArray(n,arr,k);
+    }
+
+    static void countSubArray(int n, int[] arr,int k){
+        int start = 0;
+        int end = 0;
+        int count = 0;
+        int sum = arr[0];
+
+        while (end < n && start < n){
+          
+            if(sum < k){
+                
+            	end++;
+                count += end-start;
+                
+                if(end < n)
+                    sum += arr[end];
+            } else{
+                sum -= arr[start];
+                start++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
