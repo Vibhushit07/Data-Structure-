cb264817b47af366719232efbd52c2f1f67e8a98
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-10 03:59:47 +0530

    binary tree implemented

--- tree/btree.java
+++ tree/btree.java
@@ -1,10 +1,50 @@
 package tree;
 
+
+/**
+ * 	 Tree will look like this
+ * 
+ *         2
+ *       /   \
+ *      3     5
+ *       \   /
+ *        9 7
+ */
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int data){
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinaryTree {
+	Node root;
+	
+	BinaryTree(){
+		this.root = null;
+	}
+	
+	BinaryTree(int data) {
+		this.root = new Node(data);
+	}
+}
+
 public class btree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		BinaryTree bt = new BinaryTree(2); // BinaryTree with root node 2
+		bt.root.left = new Node(3);
+		bt.root.right = new Node(5);
+		bt.root.left.right = new Node(9);
+		bt.root.right.left = new Node(7);
+		
 	}
-
 }
