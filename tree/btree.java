bc9571ad1fe8e60cb1af12e6ee1aeed6c4ad4ade
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-10 04:22:56 +0530

    level order in binary tree

--- tree/btree.java
+++ tree/btree.java
@@ -11,6 +11,8 @@ package tree;
  *        9 7
  */
 
+import java.util.*;
+
 class Node {
 	int data;
 	Node left;
@@ -33,6 +35,23 @@ class BinaryTree {
 	BinaryTree(int data) {
 		this.root = new Node(data);
 	}
+	
+	void levelOrder() {
+		Queue<Node> q = new LinkedList<>();
+		q.add(this.root);
+		
+		System.out.println("Level order of Binary Tree");
+		while(!q.isEmpty()) {
+			Node node = q.remove();
+			
+			if(node.left != null)
+				q.add(node.left);
+			if(node.right != null)
+				q.add(node.right);
+			
+			System.out.print(node.data + " ");
+		}
+	}
 }
 
 public class btree {
@@ -45,6 +64,8 @@ public class btree {
 		bt.root.right = new Node(5);
 		bt.root.left.right = new Node(9);
 		bt.root.right.left = new Node(7);
+		
+		bt.levelOrder();
 		
 	}
 }
