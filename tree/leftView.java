347b47a0cae49bb58a4d1c7b7462b767fd47c90f
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-12 00:13:54 +0530

    left view

--- tree/leftView.java
+++ tree/leftView.java
@@ -1,69 +1,69 @@
-//package tree;
-//
-///**
-// * 	 Tree will look like this
-// * 
-// *         2
-// *       /   \
-// *      3     5
-// *       \   /
-// *        9 7
-// */
-//
-//import java.util.*;
-//
-//class Node {
-//	int data;
-//	Node left;
-//	Node right;
-//	
-//	Node(int data){
-//		this.data = data;
-//		this.left = null;
-//		this.right = null;
-//	}
-//}
-//
-//class BinaryTree {
-//	Node root;
-//	
-//	BinaryTree(){
-//		this.root = null;
-//	}
-//	
-//	BinaryTree(int data) {
-//		this.root = new Node(data);
-//	}
-//	
-//	int levelPrint = 1;
-//	void view(Node root, int level) {
-//		
-//		if(root == null) return;
-//		
-//		if(levelPrint == level) {
-//			System.out.print(root.data + " ");
-//			levelPrint++;
-//		}
-//		
-//		view(root.left, level+1);
-//		view(root.right, level+1);
-//	}
-//	
-//}
-//
-//public class leftView {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		
-//		//explicit linking
-//		BinaryTree bt = new BinaryTree(2); // BinaryTree with root node 2
-//		bt.root.left = new Node(3);
-//		bt.root.right = new Node(5);
-//		bt.root.left.right = new Node(9);
-//		bt.root.right.left = new Node(7);
-//		
-//		System.out.println("Left View");
-//		bt.view(bt.root, 1);
-//	}
-//}
+package tree;
+
+/**
+ * 	 Tree will look like this
+ * 
+ *         2
+ *       /   \
+ *      3     5
+ *       \   /
+ *        9 7
+ */
+
+import java.util.*;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int data){
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinaryTree {
+	Node root;
+	
+	BinaryTree(){
+		this.root = null;
+	}
+	
+	BinaryTree(int data) {
+		this.root = new Node(data);
+	}
+	
+	int levelPrint = 1;
+	void view(Node root, int level) {
+		
+		if(root == null) return;
+		
+		if(levelPrint == level) {
+			System.out.print(root.data + " ");
+			levelPrint++;
+		}
+		
+		view(root.left, level+1);
+		view(root.right, level+1);
+	}
+	
+}
+
+public class leftView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//explicit linking
+		BinaryTree bt = new BinaryTree(2); // BinaryTree with root node 2
+		bt.root.left = new Node(3);
+		bt.root.right = new Node(5);
+		bt.root.left.right = new Node(9);
+		bt.root.right.left = new Node(7);
+		
+		System.out.println("Left View");
+		bt.view(bt.root, 1);
+	}
+}
