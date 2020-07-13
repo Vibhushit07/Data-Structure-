8ed7ae9b1f5d8bc2daa66ad5d5bf887315af88e2
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-13 14:21:22 +0530

    nth fibonacci term

---
+++ practise/Fibonacci.java
@@ -0,0 +1,26 @@
+package practise;
+
+public class Fibonacci {
+	
+	static void nthTerm(int input) {
+		
+		int a = 0, b = 1, c = 1;
+		
+		for(int i = 1; i < input; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nthTerm(4);
+		nthTerm(2);
+		nthTerm(10);
+		nthTerm(15);
+	}
+
+}
