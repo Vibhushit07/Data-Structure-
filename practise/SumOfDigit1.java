cf2ebc5ceaf71dcbeca1f3b90f83d5240095e2f4
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-13 14:52:03 +0530

    sum of even or odd digit

---
+++ practise/SumOfDigit1.java
@@ -0,0 +1,28 @@
+package practise;
+
+public class SumOfDigit1 {
+
+	static int sum(int input, char option) {
+		int sumEven = 0, sumOdd = 0;
+		
+		while(input != 0) {
+			int rem = input % 10;
+			input /= 10;
+			
+			if(rem % 2 == 0) {
+				sumEven += rem;
+			} else {
+				sumOdd += rem;
+			}
+		}
+		
+		if(option == 'e') return sumEven;
+		return sumOdd;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(sum(84001, 'e'));
+	}
+
+}
