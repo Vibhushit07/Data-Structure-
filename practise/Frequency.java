79850aec6887132a930b555779fd741006866199
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-13 14:27:01 +0530

    maximum frequency digit in a number

---
+++ practise/Frequency.java
@@ -0,0 +1,30 @@
+package practise;
+
+public class Frequency {
+	
+	static int digitFre(int x) {
+		int freq[] = new int[10];
+		int max = -1, index = -1;
+		
+		while(x != 0) {
+			int rem = x % 10;
+			x /= 10;
+			freq[rem]++;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			if(max < freq[i]) {
+				max = freq[i];
+				index = i;
+			}
+		}
+		
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(digitFre(98788987));
+	}
+
+}
