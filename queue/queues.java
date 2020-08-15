971784a326a23f82284b173db21ecfe30a9eaa63
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-16 03:17:34 +0530

    enqueue method

--- queue/queues.java
+++ queue/queues.java
@@ -21,13 +21,43 @@ class MyQueues {
 		this.rear = null;
 		this.size = 0;
 	}
+	
+	public void enqueue(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if(this.front == null) {
+			this.front = newNode;
+			this.rear = newNode;
+			
+		} else {
+			this.rear.next = newNode;
+			this.rear = newNode;
+		}
+	}
+	
+	public void printQueue() {
+		Node temp = this.front;
+		
+		while(temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		
+		System.out.println();
+	}
 }
 
 public class queues {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyQueues mq = new MyQueues();
+		
+		for(int i = 0; i < 5; i++) {
+			mq.enqueue(i);
+		}
+		
+		mq.printQueue();
 	}
-
 }
