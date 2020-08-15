cba634014f1bc195fd919999411170dbd9e220ef
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-16 03:10:43 +0530

    implemented queue using linked list

--- queue/queues.java
+++ queue/queues.java
@@ -1,5 +1,28 @@
 package queue;
 
+class MyQueues {
+	
+	private Node front;
+	private Node rear;
+	private int size;
+	
+	static class Node {
+		int data;
+		Node next;
+		
+		Node (int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	MyQueues () {
+		this.front = null;
+		this.rear = null;
+		this.size = 0;
+	}
+}
+
 public class queues {
 
 	public static void main(String[] args) {
