c8f9dc86fabe34fb9af21de82dd85112b8b6cc6d
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-06 00:22:12 +0530

    insert at beginning in circular linked list naive approach

--- circularLinkedList/CircularLinkedListt.java
+++ circularLinkedList/CircularLinkedListt.java
@@ -15,13 +15,39 @@ class MyCircularLinkedListt {
 			this.next = null;
 		}
 	}
+	
+	public void insertAtBeginNaive(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if(this.head == null) {
+			newNode.next = newNode;
+		} else {
+			Node temp = this.head;
+			
+			while(temp != this.head) {
+				temp = temp.next;
+			}
+			
+			temp.next = newNode;
+			newNode.next = this.head;
+		}
+		
+		this.head = newNode;
+	}
 }
 
 public class CircularLinkedListt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MyCircularLinkedListt cl = new MyCircularLinkedListt();
+		
+		cl.insertAtBeginNaive(1);
+		cl.insertAtBeginNaive(2);
+		cl.insertAtBeginNaive(3);
+		cl.insertAtBeginNaive(4);
 	}
 
 }
