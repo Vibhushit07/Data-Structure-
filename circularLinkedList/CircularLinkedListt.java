85fec4ae6963a94f9455b8579b5755d61d8b3fd8
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-06 00:03:46 +0530

    initialised circular linked list

---
+++ circularLinkedList/CircularLinkedListt.java
@@ -0,0 +1,27 @@
+package circularLinkedList;
+
+class MyCircularLinkedListt {
+	
+	Node head;
+	int size = 0;
+	
+	static class Node {
+		
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
+
+public class CircularLinkedListt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
