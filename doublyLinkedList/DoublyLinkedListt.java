14622ece235db9d01ec278e5ace7318dad4db2d6
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-07 05:25:15 +0530

    initialized doubly linked list

---
+++ doublyLinkedList/DoublyLinkedListt.java
@@ -0,0 +1,27 @@
+package doublyLinkedList;
+
+class MyDoublyLinkedListt {
+	
+	Node head;
+	int size = 0;
+	
+	static class Node {
+		int data;
+		Node prev;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+			next = prev = null;
+		}
+	}
+}
+
+public class DoublyLinkedListt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
