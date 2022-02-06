45f1ff03d9f2695abb42f8abe6b380ef25df5426
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-07 05:28:07 +0530

    insert at head in dll

--- doublyLinkedList/DoublyLinkedListt.java
+++ doublyLinkedList/DoublyLinkedListt.java
@@ -15,13 +15,32 @@ class MyDoublyLinkedListt {
 			next = prev = null;
 		}
 	}
+	
+	public void insertAtBegin(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if(this.head == null) {
+			this.head = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		
+		this.size++;
+	}
 }
 
 public class DoublyLinkedListt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MyDoublyLinkedListt dll = new MyDoublyLinkedListt();
+		
+		dll.insertAtBegin(1);
+		dll.insertAtBegin(2);
 	}
 
 }
