66cf66335832614e6fcfb2a9cbe2c9bfedef8318
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-08 04:08:11 +0530

    intitalized circular doubly linked list

---
+++ linkedList/CircluarDoublyLinkedListt.java
@@ -0,0 +1,27 @@
+package linkedList;
+
+class MyCircluarDoublyLinkedListt {
+	
+	Node head;
+	int size = 0;
+	
+	static class Node {
+		int data;
+		Node next;
+		Node prev;
+		
+		Node(int data) {
+			this.data = data;
+			this.next = this.prev = null;
+		}
+	}
+}
+
+public class CircluarDoublyLinkedListt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
