d8195c296a1ec016e98d50acb656a29658bd1cc2
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-08 04:26:36 +0530

    traversing in cdll

--- linkedList/CircluarDoublyLinkedListt.java
+++ linkedList/CircluarDoublyLinkedListt.java
@@ -22,14 +22,30 @@ class MyCircluarDoublyLinkedListt {
 		
 		if(this.head == null) {
 			this.head = newNode;
+			newNode.next = newNode;
+			newNode.prev = newNode;
 		} else {
 			
+			this.head.prev.next = newNode;
+			newNode.prev = this.head.prev;
 			newNode.next = this.head;
 			this.head.prev = newNode;
 			this.head = newNode;
 		}
-		
+
 		this.size++;
+	}
+	
+	public void printList() {
+		
+		Node curr = this.head;
+		
+		do {
+			System.out.print(curr.data + " ");
+			curr = curr.next;
+		} while(curr != this.head);
+		
+		System.out.println();
 	}
 }
 
@@ -37,7 +53,13 @@ public class CircluarDoublyLinkedListt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MyCircluarDoublyLinkedListt cdll = new MyCircluarDoublyLinkedListt();
+		
+		cdll.insertAtBegin(1);
+		cdll.insertAtBegin(2);
+		cdll.insertAtBegin(3);
+		cdll.printList();
 	}
 
 }
