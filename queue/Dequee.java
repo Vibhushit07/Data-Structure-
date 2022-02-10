176697f686d97623b789184a4a18d142c68f0a47
Vibhushit07 <vibhushitsinghal80@gmail.com>
2022-02-10 06:59:08 +0530

    intialized deque

---
+++ queue/Dequee.java
@@ -0,0 +1,21 @@
+package queue;
+
+class MyDequee {
+	
+	int arr[];
+	int front, size;
+	
+	public MyDequee(int cap) {
+		arr = new int[cap];
+	}
+	
+}
+
+public class Dequee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
